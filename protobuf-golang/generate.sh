7ee8bdd5985d00cf64f22ae950220e5c1d0f8b64
nhatvu148 <nhatvu148@gmail.com>
2021-07-07 14:01:24 +0900

    add addressbookpb

--- protobuf-golang/generate.sh
+++ protobuf-golang/generate.sh
@@ -1,3 +1,4 @@
 protoc -I src/ --go_out=src/ src/simplepb/simple.proto
 protoc -I src/ --go_out=src/ src/enumpb/enum.proto
 protoc -I src/ --go_out=src/ src/complexpb/complex.proto
+protoc -I src/ --go_out=src/ src/addressbookpb/addressbook.proto
--- protobuf-golang/main.go
+++ protobuf-golang/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/nhatvu148/protobuf/src/addressbookpb"
 	"github.com/nhatvu148/protobuf/src/complexpb"
 	"github.com/nhatvu148/protobuf/src/enumpb"
 	"github.com/nhatvu148/protobuf/src/simplepb"
@@ -21,6 +22,57 @@ func main() {
 	doEnum()
 
 	doComplex()
+
+	doAddressbok()
+}
+
+func doAddressbok() {
+	p := addressbookpb.Person{
+		Id:    1234,
+		Name:  "John Doe",
+		Email: "jdoe@example.com",
+		Phones: []*addressbookpb.Person_PhoneNumber{
+			{Number: "555-4321", Type: addressbookpb.Person_HOME},
+		},
+	}
+
+	ab := addressbookpb.AddressBook{
+		People: []*addressbookpb.Person{
+			&p,
+			{
+				Id:    5435,
+				Name:  "Will Doe",
+				Email: "wdoe@example.com",
+				Phones: []*addressbookpb.Person_PhoneNumber{
+					{Number: "222-3245", Type: addressbookpb.Person_HOME},
+				},
+			},
+		},
+	}
+
+	fmt.Println(ab)
+
+	fname := "address.bin"
+	// Write the new address book back to disk.
+	out, err := proto.Marshal(&ab)
+	if err != nil {
+		log.Fatalln("Failed to encode address book:", err)
+	}
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+
+	// Read the existing address book.
+	in, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	book := &addressbookpb.AddressBook{}
+	if err := proto.Unmarshal(in, book); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
+	}
+
+	fmt.Println("New book:", book)
 }
 
 func doComplex() {
