48d20109925cc13b4984ba6d896bce8209ac7584
nhatvu148 <nhatvu148@gmail.com>
2021-07-13 12:48:46 +0900

    add cm

--- protobuf-golang/generate.sh
+++ protobuf-golang/generate.sh
@@ -1,3 +1,5 @@
+# Notice: Open the correct root folder of Go projects to run protoc properly
+
 protoc -I src/ --go_out=src/ src/simplepb/simple.proto
 protoc -I src/ --go_out=src/ src/enumpb/enum.proto
 protoc -I src/ --go_out=src/ src/complexpb/complex.proto
